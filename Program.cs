c5309d438a9f2568c9c6943e1a9f467e0b80ca84
mohamedahmed10120044 <121125549+mohamedahmed10120044@users.noreply.github.com>
2022-12-20 11:21:06 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2_perfectn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {Console.WriteLine("enter the n1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the n2");
+            int n2 = int.Parse(Console.ReadLine());
+            for(int a = n1; a <= n2; a++)
+            {
+                int x = 0;
+                for (int b = 1; b < a; b++)
+                    if (a % b == 0)
+                        x += b;
+                if (a == x)
+                    Console.WriteLine(a + "  ");
+
+                
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
